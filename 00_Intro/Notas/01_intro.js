2c6f7a12443405a392d5184f1ee6468545b9e1a2
Daniel <studiosdopplermusic@gmail.com>
2022-07-06 21:08:10 -0500

    intr

---
+++ 00_Intro/Notas/01_intro.js
@@ -0,0 +1,7 @@
+/*
+¿Que es Node js 
+-  Es un entorno de ejecución de JS fuerra del navegador mediante el motor de codigo abierto V8 
+-  Es usado para el uso del JavaScript del lado del servidor
+
+
+*/ 
