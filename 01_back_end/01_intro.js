591d422812d7fc368befd2b0616d14894ce4070e
Daniel <studiosdopplermusic@gmail.com>
2022-07-13 11:17:01 -0500

    intro back

---
+++ 01_back_end/01_intro.js
@@ -0,0 +1,36 @@
+/*
+COMO FUNCIONA LA WEB?
+
+- Cliente realiza una solicitud y el servidor le dá una respuesta.
+
+PARTES DE UNA URL 
+
+- Dominio 
+- Recursos 
+- headeres: información que enviaremos junto a la solicitud
+- body: contiene la data enviada mediante la solicitud http
+
+HTTP
+- Protocolo que permite comunicación entre servidores
+
+HTTPS
+- Protocolo encriptado mediante TLS o SSL
+- Se aplican las mismas solicitudes que en HTTP tradicional 
+
+MOMENTOS DE UNA SOLICITUD
+
+- Escritura de la URL
+- Convertir URL a IP en servidor DNS (Domain Name Server)
+
+
+TCP/IP
+- Protocolos que permiten la comunicación cliente servidor
+
+TCP
+- Divide la solicitud en pequeños fragmentos y al recibirla en el servidor la reconstruye 
+
+IP
+- Enruta la solicitudes a la ubicación deseada
+
+
+*/ 
