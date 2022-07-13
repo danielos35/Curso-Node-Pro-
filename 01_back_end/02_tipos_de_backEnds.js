10e9b27ea967eeabe3124b88430a679f2d3bdfee
Daniel <studiosdopplermusic@gmail.com>
2022-07-13 11:28:50 -0500

    tipos de apis

---
+++ 01_back_end/02_tipos_de_backEnds.js
@@ -0,0 +1,13 @@
+/*
+
+SERVIDOR ESTATICO 
+- Solo es Usado para entregar archivos
+
+SERVIDOR DINAMICO
+- cuando los sitios web son entregados por un API se denomina Renderización del lado del servidor
+
+API
+- Entrega datos en formato JSON solicitados por el cliente
+- Pueden ser consumidas por diferentes clientes (No solo por navegadore)
+
+*/ 
