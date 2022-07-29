1df39ccfe2d898fb396de74600f8cd14a100bbe5
Daniel <studiosdopplermusic@gmail.com>
2022-07-29 10:33:21 -0500

    refactor

--- 04_Express/app.js
+++ 04_Express/app.js
@@ -4,29 +4,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-/*
-APP GET
-- Argumento#1 = url a visitar 
-- Argumento#2 = función a realizar cuando se entre a esa URL 
--- Callback argument#1: req, petición 
--- Callback argument#2: res, respuesta
-
-*/
-
-// app.get('/', (req, res) => {
-//   // res.status(200).send('Bienvenido al servidor');
-//   res.status(200).json({ mensaje: 'Bienvenido al servidor', app: 'natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('Esta es una solicitud post');
-// });
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getALLTours = (req, res) => {
   res.status(200).json({
     status: 'Envio exitoso',
     results: tours.length,
@@ -34,10 +17,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
-});
+};
 
-
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
 
   const id = +req.params.id;
@@ -56,51 +38,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(400).json({
-      status: 'Solicitud NO encontrada',
-      data: {
-        tour: '<error al actualizar el tour>',
-      },
-    });
-  }
-
-  res.status(200).json({
-    status: 'Solicitud exitosa',
-    data: {
-      tour: '<URL Tour actualizado>',
-    },
-  });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(400).json({
-      status: 'Solicitud NO encontrada',
-      data: {
-        tour: '<error al actualizar el tour>',
-      },
-    });
-  }
-
-  res.status(200).json({
-    status: 'Solicitud exitosa',
-    data: {
-      tour: null,
-    },
-  });
-});
-
-
-
-
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-
+const createTour = (req, res) => {
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
 
@@ -117,7 +57,59 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(400).json({
+      status: 'Solicitud NO encontrada',
+      data: {
+        tour: '<error al actualizar el tour>',
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: 'Solicitud exitosa',
+    data: {
+      tour: '<URL Tour actualizado>',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(400).json({
+      status: 'Solicitud NO encontrada',
+      data: {
+        tour: '<error al actualizar el tour>',
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: 'Solicitud exitosa',
+    data: {
+      tour: null,
+    },
+  });
+};
+
+// Lo mismo en diferente sintaxis
+// app.get('/api/v1/tours', getALLTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getALLTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
 
 // Crear servidor
 const port = 3000;
