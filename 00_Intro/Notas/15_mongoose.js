c52f40450c5cdbf5b4b3556e3979a52a0eea8985
Daniel <studiosdopplermusic@gmail.com>
2022-08-04 21:35:35 -0500

    mongoose intro

---
+++ 00_Intro/Notas/15_mongoose.js
@@ -0,0 +1,10 @@
+/*
+MONGOOSE
+
+- Biblioteca de modelado de datos par MongoDb y nodeJS. 
+- Porporciona un mayor nivel de abstracción.
+- Forma de escribir JavScript code para interactuar con la base de datos
+
+
+
+*/
