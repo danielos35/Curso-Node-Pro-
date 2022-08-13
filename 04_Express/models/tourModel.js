e77fd703d4368dd6506f92799148d4410bfb406a
Daniel <studiosdopplermusic@gmail.com>
2022-08-13 11:20:45 -0500

    modelado de datos

--- 04_Express/models/tourModel.js
+++ 04_Express/models/tourModel.js
@@ -4,17 +4,53 @@ const mongoose = require('mongoose');
 const tourShema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
+    required: [true, 'El tour debe tener un nombre'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'El tour debe tener una duración'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'El tour debe de tener un numero de personas'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'El tour debe tener una dificultad'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'El resumen del tour es requerido'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'El tour debe tener una imagen'],
+  },
+  image: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourShema);
