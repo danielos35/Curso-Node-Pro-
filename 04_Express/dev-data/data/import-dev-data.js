c984ba77213344ee687b5476c2c0a435d8571c97
Daniel <studiosdopplermusic@gmail.com>
2022-08-15 13:24:33 -0500

    finalizacion del script

--- 04_Express/dev-data/data/import-dev-data.js
+++ 04_Express/dev-data/data/import-dev-data.js
@@ -4,8 +4,7 @@ const dotenv = require('dotenv');
 
 const Tour = require('./../../models/tourModel');
 
-dotenv.config({ path: './config.env' });
-
+dotenv.config({ path: '../../config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -22,24 +21,38 @@ mongoose
   });
 
 // Leer JSON
-const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
 
 // Importar datos
 const importData = async () => {
   try {
     await Tour.create(tours);
     console.log('Datos correctamente creados');
+    process.exit();
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
 
 // Borrar todos los datos de la colleción
 const deleteData = async () => {
   try {
-    await Tour.deleteMany(tours);
-    console.log('Datos correctamente creados');
+    await Tour.deleteMany();
+    console.log('Datos correctamente eliminados');
+    process.exit();
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
