b4c2ab708ce199f4f7eb65811202d1e570cc64c4
Daniel <studiosdopplermusic@gmail.com>
2022-09-11 17:18:01 -0500

    tours por mes

--- 04_Express/controllers/tourController.js
+++ 04_Express/controllers/tourController.js
@@ -154,6 +154,20 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $unwind: '$startDates',
       },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+        },
+      },
     ]);
 
     res.status(200).json({
@@ -163,6 +177,7 @@ exports.getMonthlyPlan = async (req, res) => {
       },
     });
   } catch (err) {
+    console.log(err);
     res.status(404).json({
       status: 'Error',
       message: err,
