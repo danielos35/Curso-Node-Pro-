73c840981e9334bce8cc8f6e6184d13a5b10e140
Daniel <studiosdopplermusic@gmail.com>
2022-11-03 02:24:16 -0500

    encriptar información

--- 04_Express/models/userModel.js
+++ 04_Express/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,11 +10,13 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Indica un correo valido'],
-    unique: true,
+    unique: [true, 'El correo electronico ya se encuenrta logeado'],
     lowercase: true,
     validate: [validator.isEmail, 'Ingrese un correo valido'],
   },
   photo: String,
+
+  // NUNCA ALMACENAR LAS CONTRASENAS EN TEXTO PLANO, SIEMPRE ENCRIPTAR
   password: {
     type: String,
     required: [true, 'Ingrese una contraseña'],
@@ -22,7 +25,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Ingrese la confirmación de la contraseña'],
+    validate: {
+      // SOLO FUNCIONA EN EL GUARDADO o la creación DE LA INFORMACIÓN
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Las contraseñas ingresadas no coinciden',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Solo cuando se crea el usuario
+  if (!this.isModified('password')) return next();
+
+  // cifrado y uso de CPU
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Borrar confirmación de contraseña
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
