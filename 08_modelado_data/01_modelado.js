ec1df59c667c15f8bcc5e75e89760e69ef3f0da5
Daniel <studiosdopplermusic@gmail.com>
2022-11-20 11:17:15 -0500

    Normalizar datos

--- 08_modelado_data/01_modelado.js
+++ 08_modelado_data/01_modelado.js
@@ -25,7 +25,46 @@ EXPLICACION
 
 (La diferencia entre uno a muchos y muchos a muchos es que entre muchos a muchos la relación es en ambas direcciones, mientras que en uno a muchos es en una dirección, por ejemplo una reseña de una pelicula solo apunta a una pelicula en particular)
 
-<<<<<<<<<#1 RELACIONES ENTRE DATOS >>>>>>>>>>>>>>
+<<<<<<<<<#2 REFERENCING VS EMBEDDING >>>>>>>>>>>>>>
+
+- REFERENCIA/NORMALIZADOS 
+-- Se da cuando los datos hacen referencia a otra biblioteca de datos (video minuto 7:18)
+-- Tiene como DESVENTAJA que debemos de realizar mas consultas para optener toda la información y por tal impacta en el rendimiento de la aplicación
+
+- INCRUSTADOS/DESNORMALIZADOS (lo mas comun en bases de datos NO relacionales)
+-- Se da cuando los datos están incrustados todo en la misma biblioteca (video minuto 7:18)
+-- Tiene como VENTAJA que solo realizamos una consulta para traer toda la información
+
+- ¿COMO ELEGIR SI NORMALIZAR O NO?
+
+-- ver que relación existen entre los datos
+--- NORMALIZAR
+----- 1:POCOS
+----- 1:ALGUNOS
+----- Cuando alguno de los datos son muy pesados (como por ejemplo una imagen o un video)
+
+--- NO NORMALIZAR
+----- 1: MUCHOS
+----- 1: TONELADAS
+----- MUCHOS:MUCHOS
+
+
+-- Analizar el patron de acceso
+---- NORMALIZAR
+----- Los datos son en su mayoria solo de lectura
+----- Los datos no cambian rapidamente
+----- High read/write ratio (Hay mucha mas lectura que escritura en los datos)
+
+---- NO NORMALIZAR
+
+
+
+-- Cercania de datos
+
+
+
+
+
 
 
 
